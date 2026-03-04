92da738f69851fae3b194ef062a9e8ae72ed3962
sadiyyah1419-cmd <sadiyyah1419@gmail.com>
2026-03-04 12:27:24 +0000

    rock paper scissors.py

--- rock paper scissors.py
+++ rock paper scissors.py
@@ -1,12 +1,19 @@
 import random
 list=["rock","paper","scissors"]
+user=input("Choose rock, paper or scissors ")
 computer=random.choice(list)
-user=input("Choose rock, paper or scissors")
-if user=="rock" and computer=="rock" or user=="paper" and computer=="paper" or user=="scissors" and computer=="scissors":
-    print("draw")
-elif user=="rock" and computer=="paper" or user=="paper" and computer=="scissors" or user=="scissors" and computer=="rock":
-    print("You loose")
-elif computer=="rock" and user=="paper" or computer=="paper" and user=="scissors" or computer=="scissors" and user=="rock":
-    print("You win")
-else:
-    print("Try again")
+while user!="q":
+    if user=="rock" and computer=="rock" or user=="paper" and computer=="paper" or user=="scissors" and computer=="scissors":
+        print("draw")
+    elif user=="rock" and computer=="paper" or user=="paper" and computer=="scissors" or user=="scissors" and computer=="rock":
+        print("You loose")
+    elif computer=="rock" and user=="paper" or computer=="paper" and user=="scissors" or computer=="scissors" and user=="rock":
+        print("You win")
+    else:
+        print("Try again")
+    computer=random.choice(list)
+    user=input("Choose rock, paper or scissors ")
+
+if user=="q":
+    while True:
+        break
