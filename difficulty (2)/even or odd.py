6f924163b3a2b4246049bac4d16d77b7bf8e6842
sadiyyah1419-cmd <sadiyyah1419@gmail.com>
2026-03-05 13:50:40 +0000

    difficulty (2)/even or odd.py

--- difficulty (2)/even or odd.py
+++ difficulty (2)/even or odd.py
@@ -1,19 +1,22 @@
 import random
 number=random.randint(1,100)
 guess=input("Enter your guess ")
+score=0
 while guess!="q":
     if number%2==1 and guess=="odd" or number%2==0 and guess=="even":
-        print("You're correct")
+        score+=1
+        print(f"You're correct. The number is {number}. Your score is {score}")
     elif number%2==0 and guess=="odd":
-        print("It was even")
+        print(f"It was even. The number is {number}. Your score is {score}")
     elif number%2==1 and guess=="even":
-        print("It was odd")
+        print(f"It was odd. The number is {number}. Your score is {score}")
     else:
         print("Try again")
     number=random.randint(1,100)
     guess=input("Enter your guess ")
 
 if guess=="q":
+    print(f"Your score is {score}")
     while True:
         break
     
