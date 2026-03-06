0d5a0fcff655a32a3933d8add92a2caaee6c1f5f
sadiyyah1419-cmd <sadiyyah1419@gmail.com>
2026-03-06 11:47:43 +0000

    difficulty (1)/Completed/mad libs.py

---
+++ difficulty (1)/Completed/mad libs.py
@@ -0,0 +1,7 @@
+
+noun=input("Enter a noun ")
+verb=input("Enter a verb ")
+adverb=input("Enter an adverb ")
+
+sentence=(f"{noun} is {verb} {adverb}")
+print(sentence)
--- difficulty (1)/Incomplete/mad libs.py
+++
@@ -1,7 +0,0 @@
-
-noun=input("Enter a noun ")
-verb=input("Enter a verb ")
-adjective=input("Enter an adjective ")
-
-sentence=(f"{noun} is {verb} {adjective}")
-print(sentence)
