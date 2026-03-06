b41126af7bd13eaf7091b40eeece75fb0b80604e
sadiyyah1419-cmd <sadiyyah1419@gmail.com>
2026-03-06 11:16:14 +0000

    difficulty (1)/coin flip.py

---
+++ difficulty (2)/Incomplete/simple countdown timer.py
@@ -0,0 +1,4 @@
+timer=input("Set your timer (hours:minutes:seconds)")
+for x in range(timer):
+    print(timer)
+    timer-=1
--- difficulty (2)/simple countdown timer.py
+++
@@ -1,4 +0,0 @@
-number=int(input("What number do you want to countdown from "))
-for x in range(number):
-    print(number)
-    number-=1
